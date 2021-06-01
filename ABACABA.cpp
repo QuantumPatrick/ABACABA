757a8f3b2b283779f81c07ee8a13c9f49ae72e0f
QuantumPatrick <77089965+QuantumPatrick@users.noreply.github.com>
2021-06-01 14:36:57 -0700

    Update ABACABA.cpp

--- ABACABA.cpp
+++ ABACABA.cpp
@@ -2,17 +2,15 @@
 #include <string>
 
 int main(void) {
-    std::string abacaba = "a";
+    std::string abacaba = "a"; // initializes sequence-string as "a"
     std::string initial;
-    std::cout << abacaba << std::endl;
     int num_iterations = 26;
     char new_char;
     for (int i = 1; i < num_iterations; i++) {
-        new_char = 97 + i;
-        initial = abacaba;
+        new_char = 97 + i; // uses ASCII values to add subsequent letters
+        initial = abacaba; // saves sequence to add after new character
         abacaba += new_char;
         abacaba += initial;
-        //std::cout << abacaba << std::endl;
     }
     std::cout << abacaba << std::endl;
 }
